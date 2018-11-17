4667d3e11c15d7b955b4fb6a7d86f73837dcab60
Sully <andrew.sullivan@procore.com>
2018-11-17 15:48:15 -0800

    Add simple spec

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe ArticlesController, type: :controller do
-
+  describe 'GET #index' do
+    it 'sets the correct instance variables' do
+      get :index
+      expect(response).to render_template("index")
+    end
+  end
 end
